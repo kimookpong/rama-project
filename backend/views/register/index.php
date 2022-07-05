207dfd1af68612d8f46e86d6f75b31994de979ec
manitgithub <manit.pbac@gmail.com>
2022-07-05 11:22:11 +0700

    gii backoffice

---
+++ backend/views/register/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Registers';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="register-index">
+
+
+   
+
+
+</div>
