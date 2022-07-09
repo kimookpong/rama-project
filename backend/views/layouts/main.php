167536ed1e58b5edc7871b1ac00b520b0a8a0454
manitgithub <manit.pbac@gmail.com>
2022-07-09 16:40:57 +0700

    Update main.php

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -12,30 +12,30 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="AdminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="AdminLTE/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="AdminLTE/plugins/select2/css/select2.min.css">
-  <link rel="stylesheet" href="AdminLTE/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
 
-  <link rel="stylesheet" href="AdminLTE/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="AdminLTE/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="AdminLTE/plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/summernote/summernote-bs4.min.css">
 
-  <link rel="stylesheet" href="AdminLTE/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="AdminLTE/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="AdminLTE/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 
 </head>
 <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -53,7 +53,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="AdminLTE/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="<?=Yii::$app->params['backend']?>AdminLTE/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
@@ -125,7 +125,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="AdminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?=Yii::$app->params['backend']?>AdminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
@@ -292,55 +292,55 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="AdminLTE/plugins/jquery/jquery.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="AdminLTE/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="AdminLTE/plugins/chart.js/Chart.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="AdminLTE/plugins/sparklines/sparkline.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
   <!-- Select2 -->
-  <script src="AdminLTE/plugins/select2/js/select2.full.min.js"></script>
-  <script src="AdminLTE/plugins/datatables/jquery.dataTables.min.js"></script>
-  <script src="AdminLTE/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="AdminLTE/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="AdminLTE/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="AdminLTE/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="AdminLTE/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="AdminLTE/plugins/jszip/jszip.min.js"></script>
-<script src="AdminLTE/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="AdminLTE/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="AdminLTE/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="AdminLTE/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="AdminLTE/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+  <script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/select2/js/select2.full.min.js"></script>
+  <script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/jszip/jszip.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
-<script src="AdminLTE/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="AdminLTE/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="AdminLTE/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="AdminLTE/plugins/moment/moment.min.js"></script>
-<script src="AdminLTE/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/moment/moment.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="AdminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="AdminLTE/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="AdminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="AdminLTE/dist/js/adminlte.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/dist/js/adminlte.js"></script>
 <!-- AdminLTE for demo purposes -->
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="AdminLTE/dist/js/pages/dashboard.js"></script>
-<script src="AdminLTE/plugins/select2/js/select2.full.min.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/dist/js/pages/dashboard.js"></script>
+<script src="<?=Yii::$app->params['backend']?>AdminLTE/plugins/select2/js/select2.full.min.js"></script>
 
 </body>
 </html>
