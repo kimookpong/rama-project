b93899ad1a22b86a896bb60b45002011d946b1e4
manitgithub <manit.pbac@gmail.com>
2022-07-09 15:43:09 +0700

    update

--- frontend/views/test-the-limit/result.php
+++ frontend/views/test-the-limit/result.php
@@ -17,14 +17,14 @@ if ($model->score == 3) {
 ?>
     <div class="ad8-create text-center h-100">
         <div class="row h-100">
-            <div class="circlesuccess">
+            <div class="circlesuccessttl">
                 <h3 class="mx-4">คุณผ่านขั้นตอนเตรียมความพร้อมแล้ว</h3>
             </div>
         </div>
         <div class="container fixed-bottom">
             <div class="row">
                 <div class="col py-4 mx-auto">
-                    <a class=" w-100 btn btn-lg rounded-pill btn-brain" href="<?= Url::toRoute(['toolx/index', 'id' => $model->register_id]); ?>">หน้าถัดไป <i class="fa fa-arrow-circle-right float-end py-1" aria-hidden="true"></i></a>
+                    <a class="font-inter w-100 btn btn-lg rounded-pill btn-brain" href="<?= Url::toRoute(['toolx/index', 'id' => $model->register_id]); ?>">หน้าถัดไป <i class="fa fa-arrow-circle-right float-end py-1" aria-hidden="true"></i></a>
                 </div>
             </div>
         </div>
@@ -41,7 +41,7 @@ if ($model->score == 3) {
         <div class="container fixed-bottom">
             <div class="row">
                 <div class="col py-4 mx-auto">
-                    <a class=" w-100 btn btn-lg rounded-pill btn-brain" href="<?= Url::toRoute(['site/index', 'id' => $model->register_id]); ?>">ออก</a>
+                    <a class="font-inter w-100 btn btn-lg rounded-pill btn-brain" href="<?= Url::toRoute(['site/index', 'id' => $model->register_id]); ?>">ออก</a>
                 </div>
             </div>
         </div>
